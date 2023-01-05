f97d87e7b7f90a4a6ad825471ef808f7d94222ae
belalelmi <belalelmi@gmail.com>
2023-01-05 09:17:03 -0500

    @classmethods & @staticmethods

--- oop.py
+++ oop.py
@@ -9,10 +9,11 @@ class PlayerCharacter:
     membership = True
 
     #  __init__ is called a dunder method or a magic method
-    def __init__(self, name, age):
+    def __init__(self, name='anonymous', age='0'):
         ''' creating player attrributes '''
-        self.name = name
-        self.age = age
+        if age > 18:
+            self.name = name   # attributes
+            self.age = age
 
     def run(self):
         print('run')
@@ -20,11 +21,18 @@ class PlayerCharacter:
     def shout(self):
         print(f'my name is {self.name}')
 
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls('teddy', num1 + num2)
 
-player1 = PlayerCharacter('bhe', 81)
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
 
-print(player1.age)
-print(player1.run())
-print(player1.shout())
 
-player1.run()
+# player1 = PlayerCharacter('bhe', 10)
+# print(player1.age)
+# print(player1.run())
+print(PlayerCharacter.adding_things(2, 4))
+
+# player1.run()
