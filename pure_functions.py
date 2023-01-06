0d3cd55f19fa505d46ef5925a5c38d23d433583c
belalelmi <belalelmi@gmail.com>
2023-01-05 19:39:26 -0500

    pure functions +  map and filter

---
+++ pure_functions.py
@@ -0,0 +1,19 @@
+# def multiply_by2(listt):
+#     new_list = []
+#     for item in listt:
+#         new_list.append(item*2)
+#     return new_list
+
+def multiply_by2(item):
+    return item*2
+
+
+print(multiply_by2([2, 4, 6]))
+
+
+##################################################################################################
+
+# map, filter, zip, & reduce
+a = [1, 2, 3]
+x = map(multiply_by2, a)
+print(list(x))
