a5fc5acfa35c416e9009d1b5ce97306798335713
belalelmi <belalelmi@gmail.com>
2023-01-05 19:40:10 -0500

    pure functions +  map and filter

--- pure_functions.py
+++ pure_functions.py
@@ -1,9 +1,12 @@
-# def multiply_by2(listt):
-#     new_list = []
-#     for item in listt:
-#         new_list.append(item*2)
-#     return new_list
+# Example 1
+def multiply_by2(listt):
+    new_list = []
+    for item in listt:
+        new_list.append(item*2)
+    return new_list
 
+
+# Example 2
 def multiply_by2(item):
     return item*2
 
